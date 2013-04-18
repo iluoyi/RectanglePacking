3c8805ee4d09494ce39e76e02442027d6f5b355e
Yi <yil712@lehigh.edu>
2013-04-17 23:40:34 -0400

    Some changes for reading files and a test file.

--- src/PackSearch.java
+++ src/PackSearch.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+
 
 public class PackSearch {
+	public final int NUM_OF_RECTANGLE = 100;
 	
+	public Rectangle[] readRectangles(String fileName){
+		   BufferedReader br = null;
+		   Rectangle rList[] = null; 
+		   String line; 
+		   int n = 0;
+		   
+		    try {
+				br = new BufferedReader(new InputStreamReader(new FileInputStream(fileName)));
+				line = br.readLine();
+				n = Integer.parseInt(line);
+				
+				
+				for (int i=0; i<n; i++){
+					rList[i] = new Rectangle();
+				}
+				br.close();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}	    	
+			return rList;
+		}
 	
 }
---
+++ src/Rectangle.java
@@ -0,0 +1,32 @@
+
+public class Rectangle {
+	
+	int height, width;
+	
+	
+	public Rectangle(){
+		height = 0;
+		width = 0;
+	}
+	
+	public Rectangle(int h, int w){
+		height = h;
+		width = w;
+	}
+	
+	public void setHeight(int h){
+		height = h;
+	}
+	
+	public void setWidth(int w){
+		width = w;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+	public int getWidth(){
+		return width;
+	}
+}
